cb524a74869344c00bb9441b59b3eca2b7b63d76
coco11563 <coco11563@yeah.net>
2016-06-01 21:02:40 +0800

    first commit of Python!!!!!

---
+++ K-NN/knn.py
@@ -0,0 +1,5 @@
+'''
+Created on 2016年6月1日
+
+@author: coco1
+'''
---
+++ Test/Test_1.py
@@ -0,0 +1,11 @@
+'''
+Created on 2016年6月1日
+
+@author: coco1
+'''
+from numpy import *
+
+random.rand(4,4)
+randMat  = mat(random.rand(4,4))
+print(randMat)
+print(randMat.I)
